92ae543989ae1b63fc0a18352b49b1017e2e912a
Filip <filip.korput@gmail.com>
2021-05-27 20:54:47 +0200

    from scratch

--- aplikacja/admin.py
+++ aplikacja/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import *
 
-admin.site.register(User)
-admin.site.register(Directory)
-admin.site.register(File)
-admin.site.register(Status_Data)
-admin.site.register(Section)
+# Register your models here.
